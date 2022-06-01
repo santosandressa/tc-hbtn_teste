6ae958f0f40a3bd7cca83e3e559a75a2fb595624
santosandressa <andressasantosp0@gmail.com>
2022-06-01 14:35:47 -0300

    Task 0 - Teste Email

---
+++ 0x00/email/Pessoa.java
@@ -0,0 +1,6 @@
+public class Pessoa {
+
+    public static boolean emailValid(String email){
+        return email.contains("@") && email.length() <=50;
+    }
+}
---
+++ 0x00/email/TesteEmail.java
@@ -0,0 +1,27 @@
+public class TesteEmail{
+
+    public static boolean testar_email_com_arroba(){
+        String email = "email_teste@dominio.com.br";
+
+        Pessoa.emailValid(email);
+
+        return true;
+    }
+
+    public static boolean testar_email_sem_arroba(){
+        String email = "email_testedominio.com.br";
+
+        Pessoa.emailValid(email);
+
+        return false;
+    }
+
+    public static boolean testar_email_mais_50_caracteres(){
+        String email = "email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br";
+
+        Pessoa.emailValid(email);
+
+        return false;
+    }
+
+}
