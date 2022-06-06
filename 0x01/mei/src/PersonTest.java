6c9641966b5698c95344364a9db7d1febcf4f1af
santosandressa <andressasantosp0@gmail.com>
2022-06-06 11:11:04 -0300

    1. MEI

--- 0x01/mei/src/PersonTest.java
+++ 0x01/mei/src/PersonTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -6,7 +7,7 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@SuppressWarnings("deprecation")
 public class PersonTest {
 
     @BeforeEach
@@ -20,23 +21,27 @@ public class PersonTest {
         person.setPublicServer(true);
     }
 
+    @Test
     public void show_full_name(){
         Person person = new Person();
         assertEquals("Paul McCartney", Person.fullName(person.getName(), person.getSurname()));
     }
 
+    @Test
     public void test_calculateYearlySalary(){
         float salary = 1200;
 
         assertEquals(salary, Person.calculateYearlySalary(salary), 0);
     }
 
+    @Test
     public void person_is_MEI(){
         Person person = new Person();
 
         assertTrue(Person.isMEI(person.getSalary(), person.isAnotherCompanyOwner(), person.isPensioner(), person.isPublicServer(), person.getBirthDate()));
     }
 
+    @Test
     public void person_is_not_MEI(){
         Person person = new Person();
 
