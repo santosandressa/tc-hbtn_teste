dd903379eceb19d61f6bf5c58949653f9694502c
santosandressa <andressasantosp0@gmail.com>
2022-06-06 13:56:18 -0300

    task 0 - password

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PersonTest {
 
